f05c6d31d0668cd645e80fdaf849bf56dfb61f59
johnr <johnrubio93@gmail.com>
2019-04-11 18:23:06 +1000

    created CRUD routes for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#home"
-  get "/pages/about", to: "pages#about"
+  get "pages/about", to: "pages#about", as: "about"
+
+  get "articles", to: "articles#index", as: "articles"
+  get "articles/new", to: "articles#new", as: "new_article"
+  post "articles", to: "articles#create"
+  get "articles/:id", to: "articles#show", as: "article"
+  get "articles/:id/edit", to: "articles#edit", as: "edit_article"
+  put "articles/:id", to: "articles#update"
+  patch "articles/:id", to: "articles#update"
+  delete "articles/:id", to: "articles#destroy"
 end
