66d7de1de19369f15679a479b62ea3b5634c7ef3
johnr <johnrubio93@gmail.com>
2019-04-12 00:05:30 +1000

    fixed error in form params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,19 @@
 class ArticlesController < ApplicationController
   
   def new
-    @article = Article.new
+    # @article = Article.new
   end
   
   def create
+    @article = Article.new(article_params)
+    @article.save
     render plain: params[:article].inspect
+  end
+
+  private
+
+  def article_params
+	  params.require(:article).permit(:title, :description)
   end
 
 end
