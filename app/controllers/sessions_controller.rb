58d87c61b9d69872c973921f7055c65a99362928
johnr <johnrubio93@gmail.com>
2019-04-14 22:28:26 +1000

    fixed login create method  error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:user][:email].downcase)
+    if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       flash[:notice] = "You have successfully logged in"
       redirect_to user_path(user)
