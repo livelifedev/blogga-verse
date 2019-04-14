cc6cfdb5fa33d4cf5a2c2b0dadb7c5c44a4e6e3f
johnr <johnrubio93@gmail.com>
2019-04-14 17:50:32 +1000

    added users routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,13 @@ Rails.application.routes.draw do
   put "articles/:id", to: "articles#update"
   patch "articles/:id", to: "articles#update"
   delete "articles/:id", to: "articles#destroy"
+
+  get "users", to: "users#index", as: "users"
+  get "signup", to: "users#new", as: "signup"
+  post "users", to: "users#create"
+  get "users/:id", to: "users#show", as: "user"
+  get "users/:id/edit", to: "users#edit", as: "edit_user"
+  put "users/:id", to: "users#update"
+  patch "users/:id", to: "users#update"
+  delete "users/:id", to: "users#destroy"
 end
