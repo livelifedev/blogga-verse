326b75525d56f116a97ac73e2be444879513f22c
johnr <johnrubio93@gmail.com>
2019-04-14 22:12:35 +1000

    made user controller code more dry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
 
   def index
     @users = User.all
@@ -19,11 +20,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account successfully updated"
       redirect_to articles_path
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
+
   end
 
   private
@@ -41,4 +41,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
   
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
